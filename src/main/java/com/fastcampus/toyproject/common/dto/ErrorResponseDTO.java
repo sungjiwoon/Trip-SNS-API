774813580dd7020bb20f8ffb7ac4a6e406c27de5
sungjiwoon <nebulaisme@naver.com>
2023-11-13 01:11:58 +0900

    [fix] ErrorResponseDTO 수정

--- src/main/java/com/fastcampus/toyproject/common/dto/ErrorResponseDTO.java
+++ src/main/java/com/fastcampus/toyproject/common/dto/ErrorResponseDTO.java
@@ -1,24 +1,35 @@
 package com.fastcampus.toyproject.common.dto;
 
-import static com.fastcampus.toyproject.common.exception.ExceptionCode.INTERNAL_SERVER_ERROR;
+import static com.fastcampus.toyproject.common.exception.DefaultExceptionCode.INTERNAL_SERVER_ERROR;
 
+import com.fastcampus.toyproject.common.exception.DefaultExceptionCode;
 import com.fastcampus.toyproject.common.exception.ExceptionCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class ErrorResponseDTO {
 
-    private final ExceptionCode exceptionCode;
-    private final String errorMsg;
+    private final HttpStatus status;
+    private final String code;
+    private final String msg;
 
-    public static ErrorResponseDTO error(ExceptionCode exceptionCode, String errorMsg) {
-        return new ErrorResponseDTO(exceptionCode, errorMsg);
+    public static ErrorResponseDTO error(ExceptionCode exceptionCode) {
+        return new ErrorResponseDTO(
+                exceptionCode.getStatus(),
+                exceptionCode.getCode(),
+                exceptionCode.getMsg()
+        );
     }
 
     public static ErrorResponseDTO error() {
-        return new ErrorResponseDTO(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getMsg());
+        return new ErrorResponseDTO(
+                INTERNAL_SERVER_ERROR.getStatus(),
+                INTERNAL_SERVER_ERROR.getCode(),
+                INTERNAL_SERVER_ERROR.getMsg()
+        );
     }
 
 
