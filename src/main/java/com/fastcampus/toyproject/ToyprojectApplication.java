7790d4ed12d896e67923ff000cf4219b19a10ae9
sungjiwoon <nebulaisme@naver.com>
2023-11-13 19:34:48 +0900

    [feat] 일단 여행 컨트롤러 -> 유저 id 없어도 삽입되도록 수정

--- src/main/java/com/fastcampus/toyproject/ToyprojectApplication.java
+++ src/main/java/com/fastcampus/toyproject/ToyprojectApplication.java
@@ -2,6 +2,8 @@ package com.fastcampus.toyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 public class ToyprojectApplication {
