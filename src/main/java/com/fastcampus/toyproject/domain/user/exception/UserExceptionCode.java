4b5c9624a65e5efa08b36b436245fb607180889e
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-11-14 17:30:54 +0900

    [refactor] 에러 메시지 변경

--- src/main/java/com/fastcampus/toyproject/domain/user/exception/UserExceptionCode.java
+++ src/main/java/com/fastcampus/toyproject/domain/user/exception/UserExceptionCode.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserExceptionCode implements ExceptionCode {
 
-    EXSITED_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "존재하는 이메일"),
-    WRONG_PASSWORD(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "잘 못 된 비밀번호"),
-    NO_SUCH_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "존재 하지 않는 이메일")
+    EXSITED_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "중복 된 이메일이 있습니다."),
+    WRONG_PASSWORD(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "잘 못 된 비밀번호 입니다."),
+    NO_SUCH_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "존재 하지 않는 이메일 입니다.")
 
     ;
 
