8962830bcde5cb77a660cca112de6d163d83167f
sungjiwoon <nebulaisme@naver.com>
2023-11-15 16:24:38 +0900

    [fix] 예외처리 수정

--- src/main/java/com/fastcampus/toyproject/common/exception/DefaultExceptionCode.java
+++ src/main/java/com/fastcampus/toyproject/common/exception/DefaultExceptionCode.java
@@ -10,7 +10,7 @@ public enum DefaultExceptionCode implements ExceptionCode {
 
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR" ,"서버에 오류가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 입력값 입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 요청 입니다."),
     STARTDATE_IS_LATER_THAN_ENDDATE(HttpStatus.BAD_REQUEST, "STARTDATE_IS_LATER_THAN_ENDDATE", "출발 일정이 도착 일정보다 늦습니다."),
     ;
 
