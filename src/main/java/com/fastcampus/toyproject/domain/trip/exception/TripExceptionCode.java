01344cc0c5772a556d64de10e8c7728765bb49ab
sungjiwoon <nebulaisme@naver.com>
2023-11-15 16:36:05 +0900

    [fix] 여행예외처리코드 컨벤션 수정

--- src/main/java/com/fastcampus/toyproject/domain/trip/exception/TripExceptionCode.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/exception/TripExceptionCode.java
@@ -12,9 +12,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum TripExceptionCode implements ExceptionCode {
+
     NO_SUCH_TRIP(NOT_FOUND, "NO_SUCH_TRIP", "해당하는 여행 정보가 없습니다."),
     TRIP_ALREADY_DELETED(FORBIDDEN, "TRIP_ALREADY_DELETED", "이미 삭제된 여행 정보입니다."),
     NOT_MATCH_BETWEEN_USER_AND_TRIP(FORBIDDEN, "NOT_MATCH_BETWEEN_USER_AND_TRIP", "로그인 유저와 유저의 여행 정보가 일치하지 않습니다.")
+
     ;
 
     private final HttpStatus status;
