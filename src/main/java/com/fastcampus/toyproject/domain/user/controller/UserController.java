12683ce99695e442d6a2e5199f3029f21c4eb750
HyunA <vikim1210@naver.com>
2023-11-16 23:03:04 +0900

    [Refactor] null 대신 empty 리스트 생성

--- src/main/java/com/fastcampus/toyproject/domain/user/controller/UserController.java
+++ src/main/java/com/fastcampus/toyproject/domain/user/controller/UserController.java
@@ -2,10 +2,10 @@ package com.fastcampus.toyproject.domain.user.controller;
 
 import com.fastcampus.toyproject.common.dto.ResponseDTO;
 import com.fastcampus.toyproject.config.security.jwt.UserPrincipal;
-import com.fastcampus.toyproject.domain.liketrip.dto.LikeTripResponse;
 import com.fastcampus.toyproject.domain.trip.dto.TripDetailResponse;
 import com.fastcampus.toyproject.domain.trip.dto.TripResponse;
 import com.fastcampus.toyproject.domain.user.service.UserService;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +25,7 @@ public class UserController {
         final UserPrincipal userPrincipal) {
         return userService.getAllTrip(userPrincipal.getUserId())
             .map(tripResponses -> ResponseDTO.ok("사용자 여행 검색 완료", tripResponses))
-            /*아 여기 null이 아니라 빈 리스트 만들고 싶은데 오버라이딩 메소드 생기면서 생각대로 안 되네요*/
-            .orElse(ResponseDTO.ok("검색된 여행이 없습니다.", null));
+            .orElse(ResponseDTO.ok("검색된 여행이 없습니다.", Collections.emptyList()));
     }
 
     @GetMapping("/trip-detail/{tripId}")
