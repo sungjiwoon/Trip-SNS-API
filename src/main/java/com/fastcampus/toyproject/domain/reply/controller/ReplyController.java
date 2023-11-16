e323bf86ab170b21a366c5634b07cd24fcbbb806
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-11-16 16:19:10 +0900

    [fix] api 경로 gitbook의 경로대로 수정

--- src/main/java/com/fastcampus/toyproject/domain/reply/controller/ReplyController.java
+++ src/main/java/com/fastcampus/toyproject/domain/reply/controller/ReplyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/trip/{tripId}/reply")
+@RequestMapping("/api/trip/{tripId}/reply")
 public class ReplyController {
 
     private final ReplyService replyService;
